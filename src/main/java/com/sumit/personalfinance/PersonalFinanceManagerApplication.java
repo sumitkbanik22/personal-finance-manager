c5e355f96dd67a5be55e944091e63ee8a25e688c
Sumit Kangsa Banik <sumitk.banik22@gmail.com>
2025-08-31 10:07:57 +0530

    initial commit

---
+++ src/main/java/com/sumit/personalfinance/PersonalFinanceManagerApplication.java
@@ -0,0 +1,13 @@
+package com.sumit.personalfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinanceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinanceManagerApplication.class, args);
+	}
+
+}
